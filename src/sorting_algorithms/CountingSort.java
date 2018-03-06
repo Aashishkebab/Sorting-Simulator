8b1f675d5ba2ab93137592d248b363acfd19e1a7
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-03-06 18:04:45 -0500

    Fix comments in CountingSort

--- src/sorting_algorithms/CountingSort.java
+++ src/sorting_algorithms/CountingSort.java
@@ -1,41 +1,40 @@
 package sorting_algorithms;
 
 public class CountingSort extends Sort{
-	public static Comparable[] countingSort(Comparable arr[]) {
-		int n = arr.length;
-		int max = getMax(arr);
+	public static Comparable[] countingSort(Comparable array[]) {
+		int max = getMax(array);
 
-		// The output character array that will have sorted arr
-		Comparable output[] = new Comparable[n];
+		//The output array that will have sorted array
+		Comparable output[] = new Comparable[array.length];
 
-		// Create a count array to store count of individual
-		// characters and initialize count array as 0
+		//Create a count array to store count of individual
+		//numbers and initialize count array as 0
 		int count[] = new int[max + 1];
 		for(int i = 0; i < max; ++i) {
 			count[i] = 0;
 		}
 
-		// store count of each character
-		for(int i = 0; i < n; ++i){
-			++count[(int)arr[i]];
+		//store count of each number
+		for(int i = 0; i < array.length; ++i){
+			++count[(int)array[i]];
 		}
 
-		// Change count[i] so that count[i] now contains actual
-		// position of this character in output array
-		for (int i = 1; i <= 255; ++i){
+		//Change count[i] so that count[i] now contains actual
+		//position of this character in output array
+		for(int i = 1; i <= 255; ++i){
 			count[i] += count[i - 1];
 		}
 
-		// Build the output character array
-		for (int i = 0; i<n; ++i){
+		//Build the output array
+		for(int i = 0; i < array.length; ++i){
 			output[count[(int)arr[i]] - 1] = arr[i];
 			--count[(int)arr[i]];
 		}
 
-		// Copy the output array to arr, so that arr now
-		// contains sorted characters
-		for (int i = 0; i<n; ++i){
-			arr[i] = output[i];
+		// Copy the output array, so that it now
+		//contains sorted numbers
+		for(int i = 0; i < array.length; ++i){
+			array[i] = output[i];
 		}
 
 		return arr;
