c94163d20b97e01e5f8afa294dfec3f7fbcbc002
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-03-08 18:26:46 -0500

    Add OddEvenSort class

---
+++ src/sorting_algorithms/OddEvenSort.java
@@ -0,0 +1,33 @@
+package sorting_algorithms;
+
+public class OddEvenSort {
+	public static void oddEvenSort(int[] arr) {
+		boolean isSorted = false; // Initially array is unsorted
+		int n = arr.length;
+
+		while(!isSorted) {
+			isSorted = true;
+			int temp = 0;
+
+			// Perform Bubble sort on odd indexed element
+			for(int i = 1; i <= n - 2; i = i + 2) {
+				if(arr[i] > arr[i + 1]) {
+					temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+					isSorted = false;
+				}
+			}
+
+			// Perform Bubble sort on even indexed element
+			for(int i = 0; i <= n - 2; i = i + 2) {
+				if(arr[i] > arr[i + 1]) {
+					temp = arr[i];
+					arr[i] = arr[i + 1];
+					arr[i + 1] = temp;
+					isSorted = false;
+				}
+			}
+		}
+	}
+}
