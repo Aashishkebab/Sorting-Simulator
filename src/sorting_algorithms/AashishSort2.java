62679e1b24c77a03b6ce26032c289bc05a06f8e0
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-03-10 13:35:32 -0500

    Fix bug that stopped at index 0 AashishSort2

--- src/sorting_algorithms/AashishSort2.java
+++ src/sorting_algorithms/AashishSort2.java
@@ -4,6 +4,10 @@ public class AashishSort2 extends Sort{
 	public static int[] aashishSort2(int[] array){
 		int[] indexArray = new int[array.length];
 
+		for(int i = 0; i < indexArray.length; i++){
+			indexArray[i] = -1;
+		}
+
 		for(int i = 0; i < array.length; i++){
 			indexArray[i] = getMinIndexExcluding(array, indexArray, i);
 		}
@@ -17,13 +21,22 @@ public class AashishSort2 extends Sort{
 	}
 
 	public static int getMinIndexExcluding(int[] array, int[] exclusions, int max){
-		int mn = 0;
+		int mn = getFirstNotExcluded(exclusions);
 		for(int i = 0; i < array.length; i++){
 			if(array[i] < array[mn] && !isNumberInArray(i, exclusions, max)){
 				mn = i;
 			}
 		}
 		return mn;
+	}
+
+	public static int getFirstNotExcluded(int[] exclusions){
+		for(int i = 0; i < exclusions.length; i++){
+			if(!isNumberInArray(i, exclusions, exclusions.length)){
+				return i;
+			}
+		}
+		return exclusions.length - 1;
 	}
 
 	public static boolean isNumberInArray(int number, int[] array, int max){
