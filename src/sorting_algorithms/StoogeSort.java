fae651e9844fe99c265b292b5170127c99e3577e
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-03-09 22:56:14 -0500

    Reformat Stooge

--- src/sorting_algorithms/StoogeSort.java
+++ src/sorting_algorithms/StoogeSort.java
@@ -5,15 +5,14 @@ public class StoogeSort extends Sort{
 		threeStooges(arr, 0, arr.length - 1);
 	}
 
-	public static void threeStooges(int[] arr, int l, int h)
-	{
-		if (l >= h)
+	public static void threeStooges(int[] arr, int l, int h){
+		if(l >= h){
 			return;
+		}
 
 		// If first element is smaller
 		// than last,swap them
-		if (arr[l] > arr[h])
-		{
+		if(arr[l] > arr[h]){
 			int t = arr[l];
 			arr[l] = arr[h];
 			arr[h] = t;
@@ -21,19 +20,18 @@ public class StoogeSort extends Sort{
 
 		// If there are more than 2 elements in
 		// the array
-		if (h-l+1 > 2)
-		{
-			int t = (h-l+1) / 3;
+		if(h - l + 1 > 2){
+			int t = (h - l + 1) / 3;
 
 			// Recursively sort first 2/3 elements
-			threeStooges(arr, l, h-t);
+			threeStooges(arr, l, h - t);
 
 			// Recursively sort last 2/3 elements
-			threeStooges(arr, l+t, h);
+			threeStooges(arr, l + t, h);
 
 			// Recursively sort first 2/3 elements
 			// again to confirm
-			threeStooges(arr, l, h-t);
+			threeStooges(arr, l, h - t);
 		}
 	}
 }
