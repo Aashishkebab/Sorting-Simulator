f34e6b90b28e8f5a698e5be78fe3690228cbc0e9
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-03-11 11:15:16 -0400

    Catch OutOfMemory

--- src/Sorting_Simulator/Fork.java
+++ src/Sorting_Simulator/Fork.java
@@ -5,6 +5,7 @@
  */
 package Sorting_Simulator;
 
+import javafx.application.Platform;
 import sorting_algorithms.*;
 
 import java.time.Instant;
@@ -29,108 +30,123 @@ public class Fork implements Runnable{
 
 	@Override
 	public void run(){
-		if(this.array.length <= 1){
-			allBlocks.offer(this.array);
-			return;
-		}
+		try{
+			if(this.array.length <= 1){
+				allBlocks.offer(this.array);
+				return;
+			}
 
-		if(fork.equals("slowSort")){
-			SlowSort.slowSort(this.array);
-		}
-		if(fork.equals("cycleSort")){
-			CycleSort.cycleSort(this.array);
-		}
-		if(fork.equals("stoogeSort")){
-			StoogeSort.stoogeSort(this.array);
-		}
-		if(fork.equals("selectionSort")){
-			SelectionSort.selectionSort(this.array);
-		}
-		if(fork.equals("heapSort")){
-			HeapSort.heapSort(this.array);
-		}
-		if(fork.equals("bubbleSort")){
-			BubbleSort.bubbleSort(this.array);
-		}
-		if(fork.equals("cocktailSort")){
-			CocktailSort.cocktailSort(this.array);
-		}
-		if(fork.equals("oddEvenSort")){
-			OddEvenSort.oddEvenSort(this.array);
-		}
-		if(fork.equals("combSort")){
-			CombSort.combSort(this.array);
-		}
-		if(fork.equals("insertionSort")){
-			InsertionSort.insertionSort(this.array);
-		}
-		if(fork.equals("shellSort")){
-			ShellSort.shellSort(this.array);
-		}
-		if(fork.equals("quickSort")){
-			QuickSort.quickSort(this.array);
-		}
-		if(fork.equals("mergeSort")){
-			this.array = MergeSort.mergeSort(this.array);
-		}
+			if(fork.equals("slowSort")){
+				SlowSort.slowSort(this.array);
+			}
+			if(fork.equals("cycleSort")){
+				CycleSort.cycleSort(this.array);
+			}
+			if(fork.equals("stoogeSort")){
+				StoogeSort.stoogeSort(this.array);
+			}
+			if(fork.equals("selectionSort")){
+				SelectionSort.selectionSort(this.array);
+			}
+			if(fork.equals("heapSort")){
+				HeapSort.heapSort(this.array);
+			}
+			if(fork.equals("bubbleSort")){
+				BubbleSort.bubbleSort(this.array);
+			}
+			if(fork.equals("cocktailSort")){
+				CocktailSort.cocktailSort(this.array);
+			}
+			if(fork.equals("oddEvenSort")){
+				OddEvenSort.oddEvenSort(this.array);
+			}
+			if(fork.equals("combSort")){
+				CombSort.combSort(this.array);
+			}
+			if(fork.equals("insertionSort")){
+				InsertionSort.insertionSort(this.array);
+			}
+			if(fork.equals("shellSort")){
+				ShellSort.shellSort(this.array);
+			}
+			if(fork.equals("quickSort")){
+				QuickSort.quickSort(this.array);
+			}
+			if(fork.equals("mergeSort")){
+				this.array = MergeSort.mergeSort(this.array);
+			}
 //		if(fork.equals("timSort")){
 //			TimSort.timSort(this.array);
 //		}
-		if(fork.equals("randomSort")){
-			RandomSort.randomSort(this.array);
-		}
-		if(fork.equals("gravitySort")){
-			this.array = GravitySort.gravitySort(this.array);
-		}
-		if(fork.equals("javaSort")){
-			Arrays.sort(this.array);
-		}
-		if(fork.equals("trumpSort")){
-			this.array = TrumpSort.makeSortingGreatAgain(this.array);
-		}
-		if(fork.equals("bernieSort")){
-			BernieSort.spreadTheWealth(this.array);
-		}
-		if(fork.equals("hillarySort")){
-			HillarySort.lieToEveryone(this.array);
-		}
-		if(fork.equals("radixSort")){
-			RadixSort.radixSort(this.array);
-		}
-		if(fork.equals("bucketSort")){
-			BucketSort.bucketSort(this.array);
-		}
-		if(fork.equals("countingSort")){
-			CountingSort.countingSort(this.array);
-		}
-		if(fork.equals("pigeonSort")){
-			PigeonholeSort.pigeonholeSort(this.array);
-		}
-		if(fork.equals("aashishSort1")){
-			AashishSort1.aashishSort1(this.array);
-		}
-		if(fork.equals("aashishSort2")){
-			this.array = AashishSort2.aashishSort2(this.array);
-		}
-		if(fork.equals("aashishSort5")){
-			this.array = AashishSort5.aashishSort5(this.array);
-		}
-		if(fork.equals("aashishSort4")){
-			AashishSort4.aashishSort4(this.array);
-		}
-
-		//System.out.println("Offer: " + Arrays.toString(array));
-		try{
-			allBlocks.offer(array);
-		}catch(StackOverflowError e){
-			System.out.println(
-					"Too many values were attempted to be sorted, and there was a stack overflow.");
-		}catch(NullPointerException e){
-			if(Sort.sortingMethod.equals("hillarySort")){
-				return;
-			}else{
-				System.out.println("Null pointer. Apologies.");
+			if(fork.equals("randomSort")){
+				RandomSort.randomSort(this.array);
 			}
+			if(fork.equals("javaSort")){
+				Arrays.sort(this.array);
+			}
+			if(fork.equals("trumpSort")){
+				this.array = TrumpSort.makeSortingGreatAgain(this.array);
+			}
+			if(fork.equals("bernieSort")){
+				BernieSort.spreadTheWealth(this.array);
+			}
+			if(fork.equals("hillarySort")){
+				HillarySort.lieToEveryone(this.array);
+			}
+			if(fork.equals("gravitySort")){
+				this.array = GravitySort.gravitySort(this.array);
+			}
+			if(fork.equals("radixSort")){
+				RadixSort.radixSort(this.array);
+			}
+			if(fork.equals("bucketSort")){
+				BucketSort.bucketSort(this.array);
+			}
+			if(fork.equals("countingSort")){
+				CountingSort.countingSort(this.array);
+			}
+			if(fork.equals("pigeonSort")){
+				PigeonholeSort.pigeonholeSort(this.array);
+			}
+			if(fork.equals("aashishSort1")){
+				AashishSort1.aashishSort1(this.array);
+			}
+			if(fork.equals("aashishSort2")){
+				this.array = AashishSort2.aashishSort2(this.array);
+			}
+			if(fork.equals("aashishSort5")){
+				this.array = AashishSort5.aashishSort5(this.array);
+			}
+			if(fork.equals("aashishSort4")){
+				AashishSort4.aashishSort4(this.array);
+			}
+
+			//System.out.println("Offer: " + Arrays.toString(array));
+			try{
+				allBlocks.offer(array);
+			}catch(StackOverflowError e){
+				System.out.println(
+						"Too many values were attempted to be sorted, and there was a stack overflow.");
+			}catch(NullPointerException e){
+				if(Sort.sortingMethod.equals("hillarySort")){
+					return;
+				}else{
+					System.out.println("Null pointer. Apologies.");
+				}
+			}
+		}catch(OutOfMemoryError e){
+			Platform.runLater(new OhNoOutOfMemory());
 		}
+	}
+}
+
+class OhNoOutOfMemory implements Runnable{
+
+
+	@Override
+	public void run(){
+		SortingSimulator.alert("Fatal Error", "Out of memory",
+		      "The input size was too " + "large\nfor the chosen algorithm to handle.", "WARNING",
+		      true);
 	}
 }
