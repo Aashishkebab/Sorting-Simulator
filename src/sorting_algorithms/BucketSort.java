6c1e4640a9df009ad804a31e7270a9a6a88d138b
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-03-17 19:12:23 -0400

    Initialization to zero unecessary in Java

--- src/sorting_algorithms/BucketSort.java
+++ src/sorting_algorithms/BucketSort.java
@@ -4,9 +4,9 @@ public class BucketSort extends Sort{
 	public static void bucketSort(int[] array){
 		int[] bucket = new int[getMaxValue(array) + 1];
 
-		for(int i = 0; i < bucket.length; i++){
-			bucket[i] = 0;
-		}
+//		for(int i = 0; i < bucket.length; i++){ //May need for C++ or other languages
+//			bucket[i] = 0;
+//		}
 
 		for(int i = 0; i < array.length; i++){
 			bucket[array[i]]++;
